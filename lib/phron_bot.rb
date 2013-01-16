786d93f7af1060335b0d360bc5469008637b454f
Paul Dawson <excidere@gmail.com>
2013-01-15 23:12:14 -0600

    Updated to include randomnumber.rb

--- lib/phron_bot.rb
+++ lib/phron_bot.rb
@@ -1,5 +1,6 @@
 require_relative "phron_bot/letter_page"
 require_relative "phron_bot/dictionary"
 require_relative "phron_bot/scraper"
+require_relative "phron_bot/randomnumber"
 require_relative "phron_bot/pickword"
 require_relative "phron_bot/tweet"
